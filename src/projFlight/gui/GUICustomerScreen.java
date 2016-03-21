06caa56eb1cb3301b96ed818b597f27048892c2f
pillphil <philip.woulfe@gmail.com>
2016-03-21 23:14:36 +0000

    Removed 2nd Seat Type

--- src/projFlight/gui/GUICustomerScreen.java
+++ src/projFlight/gui/GUICustomerScreen.java
@@ -179,18 +179,6 @@ public class GUICustomerScreen extends JPanel {
 		lblHello.setBounds(380, 22, 89, 46);
 		add(lblHello);
 		
-		JComboBox<String> cboSeatLeg2 = new JComboBox<String>();
-		cboSeatLeg2.setEnabled(false);
-		cboSeatLeg2.setFont(new Font("Aharoni", Font.BOLD | Font.ITALIC, 11));
-		cboSeatLeg2.setBounds(343, 227, 115, 20);
-		add(cboSeatLeg2);
-		
-		JLabel label = new JLabel("Seat Type");
-		label.setEnabled(false);
-		label.setFont(new Font("Aharoni", Font.BOLD | Font.ITALIC, 11));
-		label.setBounds(343, 202, 67, 14);
-		add(label);
-		
 		btnHelp = new JButton("Help");
 		btnHelp.setBounds(398, 300, 60, 23);
 		btnHelp.addActionListener(event);
